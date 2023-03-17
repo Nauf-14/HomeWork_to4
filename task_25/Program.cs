fc9d6aa90e5235c65018b986c19f8f2b45e89968
Naufal <nauf1207@mail.ru>
2023-03-18 00:46:09 +0600

    done Task 25

---
+++ task_25/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+for (int i = 1; i <= B; i++)
+  count = count * A;
+Console.WriteLine(count);
