f3dafd312819d0f2d843be1c2e728f63f6df853d
Naufal <nauf1207@mail.ru>
2023-03-18 14:13:02 +0600

    done task 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (a > 0)
+{
+  int count = a % 10;
+  a = a / 10;
+  sum = sum + count;
+}
+Console.WriteLine("Ответ: " + sum);
